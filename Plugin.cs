a69d2b2a6611e2729dbedc51ff7f01a468fd05fb
uu123bb <89284093+uu123bb@users.noreply.github.com>
2025-12-27 16:48:59 +0800

    注册提醒提供方（半成品）

--- Plugin.cs
+++ Plugin.cs
@@ -3,7 +3,9 @@ using ClassIsland.Core.Abstractions;
 using ClassIsland.Core.Abstractions.Services;
 using ClassIsland.Core.Attributes;
 using ClassIsland.Core.Controls;
+using ClassIsland.Core.Extensions.Registry;
 using ClassIsland.Shared;
+using ClassIsland.Shared.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
@@ -15,6 +17,7 @@ namespace RainyBird
     {
         public override void Initialize(HostBuilderContext context, IServiceCollection services)
         {
+            services.AddNotificationProvider<MyNotificationProvider>(); // 注册自定义提醒提供方
             var UriServiceReg = IAppHost.GetService<IUriNavigationService>(); // 获取 Uri 导航服务
             UriServiceReg.HandlePluginsNavigation( // 注册插件主机 Uri 服务
                 "rainybird/hello", // Uri 路径 (Hello World)

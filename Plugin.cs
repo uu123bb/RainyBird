ed626f24616898eb982e7a953ef656898926cf8d
uu123bb <lingyu3184@outlook.com>
2025-12-27 13:39:11 +0800

    添加项目文件。

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using ClassIsland.Core;
+using ClassIsland.Core.Abstractions;
+using ClassIsland.Core.Attributes;
+using ClassIsland.Core.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace RainyBird
+{
+    [PluginEntrance]
+    public class Plugin : PluginBase
+    {
+        public override void Initialize(HostBuilderContext context, IServiceCollection services)
+        {
+            AppBase.Current.AppStarted += async (_, _) =>
+                await CommonTaskDialogs.ShowDialog("Hello world!", "Hello from RainyBird!");
+        }
+    }
+}
